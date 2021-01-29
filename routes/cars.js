01e8ee9081535fb7a036d895c851f7a5391e49dd
Scott H <zenwattage@gmail.com>
2021-01-29 10:48:59 -0800

    logging cars.json to console

--- routes/cars.js
+++ routes/cars.js
@@ -1,4 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
+const app = express();
+const cars = require('../data/cars.json');
+// const path = require('path');
+// app.use(express.static(path.join(__dirname, 'data')));
+
+console.log(JSON.stringify(cars, null, "\t"));
+// app.get('/cars', function (req, res, next) {
+
+//     let rawdata = fs.readFileSync(cars);
+//     let car = JSON.parse(rawdata);
+//     console.log(car);
+//     res.send(console.log(JSON.stringify(cars)));
+
+// });
+
+
 module.exports = router;
