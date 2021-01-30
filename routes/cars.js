476e319203fde087b5c4b8fab860d7bce4b66da2
Scott H <zenwattage@gmail.com>
2021-01-30 11:27:31 -0800

    removing json formatting

--- routes/cars.js
+++ routes/cars.js
@@ -5,7 +5,7 @@ const app = express();
 const cars = require('../data/cars.json');
 
 router.get('/', function (req, res, next) {
-    res.send(JSON.stringify(cars, null, "\t"));
+    res.send(JSON.stringify(cars));
 });
 
 
