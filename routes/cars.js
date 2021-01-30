82289bb0f596cc2f2f1534d0b56fa2eb02729536
Scott H <zenwattage@gmail.com>
2021-01-29 22:05:06 -0800

    /cars route working and returning cars.json

--- routes/cars.js
+++ routes/cars.js
@@ -3,18 +3,9 @@ const router = express.Router();
 
 const app = express();
 const cars = require('../data/cars.json');
-// const path = require('path');
-// app.use(express.static(path.join(__dirname, 'data')));
 
-console.log(JSON.stringify(cars, null, "\t"));
-// app.get('/cars', function (req, res, next) {
-
-//     let rawdata = fs.readFileSync(cars);
-//     let car = JSON.parse(rawdata);
-//     console.log(car);
-//     res.send(console.log(JSON.stringify(cars)));
-
-// });
-
+router.get('/', function (req, res, next) {
+    res.send(JSON.stringify(cars, null, "\t"));
+});
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path');
-const fullPath = path.join(__dirname, "./data/cars.json");
+
+
 /* simple GET request. */
 router.get('/', function (req, res, next) {
   res.send("It's working, yo!!\n" + new Date().toISOString());
