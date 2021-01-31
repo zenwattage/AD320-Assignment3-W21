57b5d36dc2d2c4dcf36e0b27ce79902bbcf959b7
Scott H <zenwattage@gmail.com>
2021-01-31 14:30:53 -0800

    id route working

--- routes/cars.js
+++ routes/cars.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-const app = express();
+//const app = express();
 const cars = require('../data/cars.json');
 
-router.get('/', function (req, res, next) {
-    res.send(JSON.stringify(cars));
+router.get('/', (req, res, next) => {
+    console.log(querystring.stringify());
+    res.json(cars);
+});
+
+router.get('/:id', (req, res, next) => {
+
+    res.json(cars.filter(car => car.id == parseInt(req.params.id)));
+
 });
 
 
